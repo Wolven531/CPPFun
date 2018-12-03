f2283d411edde2095a8fdd41691b4a062a0ca899
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-03 14:56:42 -0700

    Setup for Fibonacci implementation

---
+++ CPPFun/Fibonacci.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Fibonacci.h"
+
+int Fibonacci::GetNthDigit(int n)
+{
+	return 0;
+}
---
+++ CPPFun/Fibonacci.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Fibonacci
+{
+public:
+	int GetNthDigit(int);
+};
+
