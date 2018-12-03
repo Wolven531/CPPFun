e4ad4ea57f24c90154ddc411c5b06ba279a6cfd2
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-02 19:14:11 -0700

    Clean up CPPFun file

--- CPPFun/CPPFun.cpp
+++ CPPFun/CPPFun.cpp
@@ -6,42 +6,7 @@
 #include <sstream>
 
 #include "CPPFun.h"
-
-// TODO: search for way to shorten this simple function definition
-void Prompter::displayGreeting(string greeting)
-{
-	cout << greeting << this->getNewLine();
-}
-
-string Prompter::getNewLine()
-{
-	// NOTE: keep newLine variable in top scope for re-use
-	string newLine;
-	{
-		// NOTE: we drop newLineCharStream to lower scope so it is cleared after usage
-		ostringstream newLineCharStream;
-		newLineCharStream << endl;
-		newLine = newLineCharStream.str();
-	}
-	return newLine;
-}
-
-string Prompter::promptForString(string prompt, bool acceptsBlank)
-{
-	string userEntry;
-	{
-		cout << prompt << this->getNewLine();
-		if (acceptsBlank) {
-			// NOTE: can accept blank input
-			getline(cin, userEntry);
-		}
-		else {
-			// NOTE: will not return until a value is entered
-			cin >> userEntry;
-		}
-	}
-	return userEntry;
-}
+#include "Prompter.h"
 
 int main()
 {
@@ -50,7 +15,7 @@ int main()
 	// Prompter::displayGreeting("Hello!");
 	// Prompter::getNewLine()
 	prompter.displayGreeting("Hello!");
-	auto userEntry(prompter.promptForString("Please enter a value and press [Enter]"));
+	auto userEntry(prompter.promptForString("Please enter a value and press [Enter]", false));
 	cout << "User entered: '" << userEntry << "'" << prompter.getNewLine();
 
 	return 0;
--- CPPFun/CPPFun.h
+++ CPPFun/CPPFun.h
@@ -1,12 +1,1 @@
 #pragma once
-
-#include <string>
-
-using namespace std;
-
-class Prompter {
-	public:
-		string getNewLine();
-		void displayGreeting(string);
-		string promptForString(string, bool = true);
-};
