dac6a0cd6300d82c71623bedf2fe1ae6a308a84d
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-04 16:32:52 -0700

    Implement the Fibonacci algorithm iteratively in C++

--- CPPFun/Fibonacci.cpp
+++ CPPFun/Fibonacci.cpp
@@ -3,5 +3,20 @@
 
 int Fibonacci::GetNthDigit(int n)
 {
-	return 0;
+	auto result(1);
+
+	if (n < 3) {
+		return result;
+	}
+
+	auto num1(1);
+	auto num2(1);
+
+	for (auto i(3); i <= n; i++) {
+		result = num1 + num2;
+		num1 = num2;
+		num2 = result;
+	}
+
+	return result;
 }
